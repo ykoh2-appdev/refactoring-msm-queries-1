e03f18fc5bca0368d2efe8f78774f0d886691ffc
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-27 15:56:05 +0000

    done

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,14 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  def movie
+    my_id = self.movie_id 
+    
+    matching_movies = Movie.where({ :id => my_id })
+
+    the_movie = matching_movies.at(0)
+
+    return the_movie
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,17 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  def director
+    my_director_id = self.director_id
+
+    matching_directors = Director.where({ :id => my_director_id })
+
+    the_director = matching_directors.at(0)
+
+    return the_director
+  end
+
+
+
 end
